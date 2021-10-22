0513410dbeb5c6551cccf49c045a50ab57ec53af
LeoAcosta07 <leoacosta07@gmail.com>
2021-10-22 12:15:43 -0300

    0.4

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 const Api = {
-    apiUrl: 'http://localhost:3001/tarefas',
+    apiUrl: 'https://projetobluebackend.herokuapp.com/tarefas',
     fetchGetAll: () => fetch(Api.apiUrl),
     fetchGetById: id => fetch(`${Api.apiUrl}/${id}`),
     fetchPost: (tarefa) => {
